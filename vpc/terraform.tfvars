a200b968b4c34a89d4d8c78f421fb219ea99eaf3
vince <vince@lab02.org>
2017-08-23 11:13:36 +0200

    introduce terraform.tfvars

---
+++ vpc/terraform.tfvars
@@ -0,0 +1,6 @@
+region = "eu-west-1"
+vpc_cidr_block = "10.0.0.0/16"
+subnet1_cidr_block = "10.0.1.0/24"
+subnet2_cidr_block = "10.0.2.0/24"
+subnet1_az = "eu-west-1a"
+subnet2_az = "eu-west-1b"
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,23 +1,11 @@
-variable "region" {
-  default = "eu-west-1"
-}
+variable "region" {}
 
-variable "vpc_cidr_block" {
-  default = "10.0.0.0/16"
-}
+variable "vpc_cidr_block" {}
 
-variable "subnet1_cidr_block" {
-  default = "10.0.1.0/24"
-}
+variable "subnet1_cidr_block" {}
 
-variable "subnet2_cidr_block" {
-  default = "10.0.2.0/24"
-}
+variable "subnet2_cidr_block" {}
 
-variable "subnet1_az" {
-  default = "eu-west-1a"
-}
+variable "subnet1_az" {}
 
-variable "subnet2_az" {
-  default = "eu-west-1b"
-}
+variable "subnet2_az" {}
